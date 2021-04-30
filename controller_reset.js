20ec082d9f5574b658fdb0161d2bb49ce636d703
NESC <NESC@DEHBNBNESC01.bachmann.at>
2021-04-30 16:15:40 +0200

    SAPIW: implemented "create offline device"

--- controller_reset.js
+++ controller_reset.js
@@ -7,7 +7,7 @@ include('utils.js')
 
 // Choice CPU from Navigator
 device = getDevice();
-
+print(device)
 // Get CPU Info
 function getCPUType(mioType, mioVariant){
 	print(mioType)
@@ -23,17 +23,20 @@ mbases = getInstalledCatalogs();
 selection = showSelectionDialog(mbases.toArray(), 'Choose a mbase version to be installed', 'Choose mbase version');
 
 catalog = getCatalog(selection)
-print(catalog.path)
-print(catalog.vers)
 unzip(catalog.path, "c:/temp/catalog/")
 
+mem = device.getController().onlineModel.deviceInfo.getGeneralInfo().getBootDevice().replace("/","").replace("/","")
+devAdr = device.getController().onlineModel.deviceInfo.m1Controller.connectionInfo.address
+devType = getCpuName(CPUInfo.mioType, CPUInfo.mioVariant)
+bootDev = device.getController().onlineModel.deviceInfo.getGeneralInfo().getBootDevice().replace("/","").replace("/","")
 
 // Ask create Offline Device yes/no
-
 if (showQuestionDialog("Would you like to create a offline device,\nbefore reset it?", "Backup")){
-	print("yes")
-	createOfflineDevice(Object container, String name, String ip, String cpuName, String[] memoryMedium, String bootDevice) : Device;
-
+	devName = showInputDialog("Please enter a name of the offline device!", 'M200', 'Offline device name')	
+	// TODO: make sure a link to a solution project will be used in first parameter, even if sub folder is selected
+	createOfflineDevice(showFolderSelectionDialog('workspace://')+'/_templates', devName, devAdr, devType, [mem, "nvram0"], bootDev) // Add additional memories if needed
+	// TODO: Copy all files:
+	
 // -yes Create Offline Device
 }
 
--- utils.js
+++ utils.js
@@ -1,32 +1,67 @@
-function getCatalog(selection){
-	var version  = java.lang.System.getProperty("SolutionCenter.version").replace(" ","");
-	version = version.substr(0,version.search("Branch"))
+	function getCatalog(selection) {
+		var version = java.lang.System.getProperty("SolutionCenter.version")
+				.replace(" ", "");
+		version = version.substr(0, version.search("Branch"))
 	
-	mbase = selection.slice(7)
+		mbase = selection.slice(7)
 	
-	if(mbase == "4.00 Release")
-		mbase = "4_00r99";
-	else if(mbase == "4.10 Release")
-		mbase = "4_10r99";
-	else if (mbase.endsWith("Release"))
-		mbase = mbase.replace(" Release","_99")
-	else
-		mbase = mbase.replace(" MS","_")
-		
-	mbase = mbase.replace(".","_").split("_")
-	if(mbase[2]){
-		if(mbase[2].length == 1)
-			mbase[2] = "0"+mbase[2]
-		mbase = mbase[0]+"_"+mbase[1]+"_"+mbase[2]
+		if (mbase == "4.00 Release")
+			mbase = "4_00r99";
+		else if (mbase == "4.10 Release")
+			mbase = "4_10r99";
+		else if (mbase.endsWith("Release"))
+			mbase = mbase.replace(" Release", "_99")
+		else
+			mbase = mbase.replace(" MS", "_")
+	
+		mbase = mbase.replace(".", "_").split("_")
+		if (mbase[2]) {
+			if (mbase[2].length == 1)
+				mbase[2] = "0" + mbase[2]
+			mbase = mbase[0] + "_" + mbase[1] + "_" + mbase[2]
+		} else
+			mbase = mbase[0] + "_" + mbase[1]
+	
+		var catalogs = findFiles(
+				"at.bachmann.app.dman.systemcatalog" + mbase + "*",
+				"C:/bachmann/SolutionCenter/" + version + "/plugins")
+		if (!catalogs[0]) {
+			showErrorDialog("Could not find any catalog with " + selection
+					+ "!\nPlease restart and choose another one!")
+			return -1
+		} else
+			return {
+				path : catalogs[0],
+				vers : mbase
+			}
 	}
-	else
-		mbase = mbase[0]+"_"+mbase[1]
 	
-	var catalogs =  findFiles("at.bachmann.app.dman.systemcatalog"+mbase+"*","C:/bachmann/SolutionCenter/"+version+"/plugins")
-	if(!catalogs[0]){
-		showErrorDialog("Could not find any catalog with " + selection +"!\nPlease restart and choose another one!")
-		return -1
+	function getCpuName(type, variant) {
+		switch (type) {
+		case 49: 
+			return 'MPC2XX'
+		case 50: 
+			return 'MX2XX'
+		case 97: 
+			switch (variant) {
+			case 2:	
+				return 'MC206/212';
+			case 3:
+				return 'MC206/212';
+			case 0:
+				return 'MC205/210';
+			case 1:
+				return 'MC205/210';
+			case 4:
+				return 'MC220';
+			}
+		case 82:
+			switch (variant) {
+			case 0 - 1:
+				return 'MH212'
+			case 2:
+				return 'MH230'
+			}
+		}
+	
 	}
-	else
-		return {path:catalogs[0],vers:mbase}
-}
